2d2d97b9dcecbe1a5eda577d3328d991917c2f4d
Alan Turing <freemasl.sf@gmail.com>
2020-01-15 13:43:21 -0700

    Test snapshot for all reservations component

---
+++ src/Reservations/Reservations.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Reservations from './Reservations';
+
+describe('App', () => {
+  let wrapper;
+  const reservationsMock = [{
+    name: 'Sam',
+    date: '1/14',
+    time: '16:00',
+    number: 2,
+    id: 1
+  }]
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <Reservations
+        reservations={reservationsMock} />)
+  })
+
+  it('should match the snapshot with all of the data passed through', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+})
