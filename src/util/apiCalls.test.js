9562d37670965234d08632f898329e9ea7df39d3
Alan Turing <freemasl.sf@gmail.com>
2020-01-17 09:14:17 -0700

    Test GET for all reservations with the correct URL

---
+++ src/util/apiCalls.test.js
@@ -0,0 +1,29 @@
+import { getReservations } from './apicalls';
+
+describe('getReservations', () => {
+  let mockReservations;
+
+  beforeEach(() => {
+    mockReservations = [{
+      name: 'Sam',
+      date: '1/14',
+      time: '16:00',
+      number: 2,
+      id: 1
+    }]
+
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockReservations)
+      })
+    })
+  });
+
+  it('should call fetch with the correct url', () => {
+    getReservations();
+
+    expect(window.fetch).toHaveBeenCalledWith('http://localhost:3001/api/v1/reservations')
+  });
+
+});
