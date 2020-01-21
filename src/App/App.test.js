5f739e35461496f0118aefa8c3e911057910525d
Alan Turing <freemasl.sf@gmail.com>
2020-01-21 11:30:32 -0700

    Mock out the api calls in App test file

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import { getReservations, postReservation } from '../util/apicalls';
+
+jest.mock('../../util/apiCalls');
+
+getReservations.mockImplementation(() => {
+  return Promise.resolve([{
+    name: 'Sam',
+    date: '1/14',
+    time: '16:00',
+    number: 2,
+    id: 1
+  }])
+});
+
+postReservation.mockImplementation(() => {
+  return Promise.resolve({
+    name: 'Sam',
+    date: '1/14',
+    time: '16:00',
+    number: 2,
+    id: 1
+  })
+});
 
 describe('App', () => {
   let wrapper;
@@ -12,4 +35,6 @@ describe('App', () => {
   it('should match the snapshot with all of the data passed through', () => {
     expect(wrapper).toMatchSnapshot();
   })
+
+
 })
