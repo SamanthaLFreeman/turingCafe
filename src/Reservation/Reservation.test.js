53448a78ac2bd2285260a4ff08e5e81d3eebae0c
Alan Turing <freemasl.sf@gmail.com>
2020-01-24 09:42:03 -0700

    Test that the cancel button calls the cancelReservation method and passes the current id

--- src/Reservation/Reservation.test.js
+++ src/Reservation/Reservation.test.js
@@ -10,13 +10,21 @@ describe('Reservation', () => {
     time: '16:00',
     number: 2
   }
+  const cancelReservationMock = jest.fn();
+
   beforeEach(() => {
     wrapper = shallow(
     <Reservation 
-      reservation={reservationMock} />)
+      reservation={reservationMock}
+        cancelReservation={cancelReservationMock} />)
   })
 
   it('should match the snapshot with all of the data passed through', () => {
     expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should call cancelReservation with the current id passed through on click of the button', () => {
+    wrapper.find('.cancel-btn').simulate('click');
+    expect(cancelReservationMock).toBeCalledWith(reservationMock.id)
   })
 })
